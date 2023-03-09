1d09686ebac12d35951c4a399c1100eaa2be9d3b
bosilveira <bosilveira@gmail.com>
2023-03-09 13:37:37 -0300

    feat: home and redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import omdbReducer from './omdbSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    omdb: omdbReducer,
   },
 });
 
